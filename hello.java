37a4d5e02723794441101f7911f491814a8e0923
Alok Singh Thakur <aloksinghthakurcse@gmail.com>
2024-04-13 22:56:04 +0530

    type of variable in java

--- hello.java
+++ hello.java
@@ -6,8 +6,19 @@ public class hello {
     public static void main(String a[]){
 
         int num1 = 3;
-        int num2 = 5;
-        int result = num1 + num2;
-        System.out.println("result "+ result);
+        byte by = 123;
+        short sh = 558;
+
+        long l = 5854l;
+
+        float f= 5.8f;
+        double d = 5.8;
+
+        char c = 'k';
+
+        boolean b = true;
+
+
+
     }
 }
